5264afcf7784c5de9ed3dfef52596124142b2d65
ANBProGamer <50081774+ANBProGamer@users.noreply.github.com>
2021-03-02 08:58:44 -0800

    Update triangleLoop.py

--- triangleLoop.py
+++ triangleLoop.py
@@ -1,40 +1,9 @@
-import random
-import turtle
-debug = 0
-t = turtle.Pen()
-
-def drawDot(coord1, coord2, sizeD, colorD):
-        t.up()
-        t.goto(coord1, coord2)
-        t.down()
-        t.dot(sizeD, colorD)
-             
-
-my_list = []
-
-# Picks three co-ordinates to form the triangle
-
-    
-y = (-300)
-z = (300)
-my_list.append([y,y])
 my_list.append([z,z])
 my_list.append([y,z])
 drawDot(z,z,20,"red")
 drawDot(y,y,20,"red")
 drawDot(y,z,20,"red")
-#t.up()
-#t.goto(z,z)
-#t.down()
-#t.dot(20,"red")
-##t.up()
-##t.goto(y,y)
-##t.down()
-##t.dot(20,"red")
-##t.up()
-##t.goto(y,z)
-##t.down()
-##t.dot(20,"red")
+
 if(debug):
         print(my_list)
 
@@ -45,10 +14,7 @@ b = random.randint(-100,100)
 if(debug):
         print(a, b)
 drawDot(a,b,5,"maroon")
-##t.up()
-##t.goto(a,b)
-##t.down()
-##t.dot(5,"green")
+
 turtle.tracer(0, 0)
 colorList = ["red","orange","yellow","green","blue","purple","pink","black","brown"]
 for l in range(0,20000):
@@ -66,11 +32,9 @@ for l in range(0,20000):
                 print(midPoint)
         color = random.choice(colorList)
         drawDot((a+i)/2, (b+j)/2,3, color)
-##        t.up()
-##        t.goto( [(a+i)/2, (b+j)/2])
-##        t.down()
-##        t.dot(3,random.choice(colorList))
+
         # Changing starting coordinates into midPoint
         a,b = midPoint
 turtle.update()
+
 
