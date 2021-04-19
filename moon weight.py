35ec52f6631d806fa76b6b078a8b4cf4048b2751
ANBProGamer <50081774+ANBProGamer@users.noreply.github.com>
2021-04-19 11:09:13 -0700

    Update moon weight.py
    added loop in function

--- moon weight.py
+++ moon weight.py
@@ -1,38 +1,11 @@
  
-for i in range(0,1):
-    
-    def WeightMoon(Weight,yearWeight):
+def WeightMoon(Weight,yearWeight):
+    for i in range(0,15):
         ((Weight) + (yearWeight))*0.165
         z = ((Weight) + (yearWeight))*0.165
         print(z)
-        a = ((Weight) + ((yearWeight) + 1))*0.165
-        print(a)
-        b = ((Weight) + ((yearWeight) + 2))*0.165
-        print(b)
-        c = ((Weight) + ((yearWeight) + 3))*0.165
-        print(c)
-        d = ((Weight) + ((yearWeight) + 4))*0.165
-        print(d)
-        e = ((Weight) + ((yearWeight) + 5))*0.165
-        print(e)
-        f = ((Weight) + ((yearWeight) + 6))*0.165
-        print(f)
-        g = ((Weight) + ((yearWeight) + 7))*0.165
-        print(g)
-        h = ((Weight) + ((yearWeight) + 8))*0.165
-        print(h)
-        i = ((Weight) + ((yearWeight) + 9))*0.165
-        print(i)
-        j = ((Weight) + ((yearWeight) + 10))*0.165
-        print(j)
-        k = ((Weight) + ((yearWeight) + 11))*0.165
-        print(k)
-        l = ((Weight) + ((yearWeight) + 12))*0.165
-        print(l)
-        o = ((Weight) + ((yearWeight) + 13))*0.165
-        print(o)
-        p = ((Weight) + ((yearWeight) + 14))*0.165
-        print(p)
-    WeightMoon(90,0)
+        yearWeight += 1
     
-        
+    
+WeightMoon(90,0)
+            
