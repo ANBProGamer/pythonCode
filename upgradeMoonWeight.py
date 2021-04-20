0c6f02f6a158502bb3760faad693179b03a615fc
ANBProGamer <50081774+ANBProGamer@users.noreply.github.com>
2021-04-20 10:09:41 -0700

    Create upgradeMoonWeight.py

---
+++ upgradeMoonWeight.py
@@ -0,0 +1,11 @@
+ 
+def WeightMoon(Weight,yearWeight,years):
+    for i in range(0,years):
+        ((Weight) + (yearWeight))*0.165
+        z = ((Weight) + (yearWeight))*0.165
+        print(z)
+        yearWeight += 1
+    
+    
+WeightMoon(90,0,37)
+            
